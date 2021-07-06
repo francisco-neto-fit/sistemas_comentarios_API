2b7cee672f30e249cc6fa053f6d94610e5681c8f
franciscogneto <francisco_gneto@hotmail.com>
2021-07-05 22:47:51 -0300

    Tag Entity

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -3,7 +3,8 @@ import {CreateUserService} from '../services/CreateUserService'
 
 
 class CreateUserController {
-    async handle(request: Request, response: Response){
+    async handle(request: Request, response: Response){ // express por padrão não tem suporte para erros assíncronos, sendo assim precisa baixar a biblioteca 'express-async-errors'
+        
         const {name, email, admin} = request.body;
         const createUserService = new CreateUserService();
         const user = await createUserService.execute({name,email,admin});
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
-import express from 'express';
+import express, {Request,Response,NextFunction} from 'express';
 import 'reflect-metadata'; 
 import './database';
+import 'express-async-errors'; // para lançar execoes assincronas, por isso fica antes das rotas
 import {router} from './routes';
+ //lidar com erros
 const app = express();
 app.use(express.json()); //indica para o exporess que será usado json
 
 app.use(router); // inserindo as rotas no express
 
+//Middleware pra erro recebe 4 parametros, next: caso queira passa para um próximo nível, ex: caso queira executa outra função
+app.use((err: Error, request: Request, response:Response, next: NextFunction)=>{
+    if(err instanceof Error){ // tudo que for do Throw new Error
+        return response.status(400).json({
+            error:err.message
+        });
+    }
+    return response.status(500).json({
+       status:'error',
+       message: "Internal Server Error" 
+    });
+});
+
 app.listen(3000, () => console.log('server is running grr')); // inicializa o servidor na porta 3000
+
+// pra criar nova migration: yarn typeorm migration:create -n CreateTags
